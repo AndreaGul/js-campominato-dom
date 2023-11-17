4ce816c139142f3974e16add5acd9efd884ff277
AndreaGul <andreagulino99@gmail.com>
2023-11-17 02:47:38 +0100

    feat:16 numeri random

--- js/main.js
+++ js/main.js
@@ -6,6 +6,12 @@
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l'utente clicca su ogni cella, la cella cliccata si colore di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 // e aggiungiamo la logica del gioco
+// Il gioco
+
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta:
+// passiamo il numero di caselle generate
+// le bombe. :bomba:
+// :esclamazione:Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
 
 function createElement(tag, classname, content) {
   const element = document.createElement(tag);
@@ -15,6 +21,25 @@ function createElement(tag, classname, content) {
   return element;
 }
 
+// in un funzione generiamo 16 numeri che andiamo a mettere in un array, questi numeri saranno compresi tra il numero di caselle generate e non potranno ripetersi
+
+function numRandom() {
+  //geriamo 16 numeri
+  const numbers = [];
+
+  do {
+    let number = Math.floor(Math.random() * 100 + 1);
+
+    // Quindi nella generazione ed inserimento di questi 16 numeri andremo a controllare nell'array prima se sono contenuti (includes()) nell''array dei numeri random e se non sono presenti li andiamo ad aggiungere con push.
+
+    if (numbers.includes(number) === false) {
+      numbers.push(number);
+    }
+  } while (numbers.length !== 16);
+
+  return numbers;
+}
+console.log(numRandom());
 // function changeColor(linkClassname, classname) {
 //   const element = document.querySelector(linkClassname);
 //   element.classList.add(classname);
